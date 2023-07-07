0950d23d90464829d103a8d4cbac48283a9aefbd
Elijah <chumpatrol1@gmail.com>
2023-07-07 10:21:04 -0400

    added skeleton test suite

---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual('Gucci', 'Gucci')
+
+if __name__ == "__main__":
+    unittest.main()
