2391781c0496f9abc31b3beae436840a590f4920
Elijah <chumpatrol1@gmail.com>
2023-07-07 10:56:44 -0400

    Added the important functions

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ class TestStringMethods(unittest.TestCase):
     def test_something(self):
         self.assertEqual('Gucci', 'Gucci')
 
+    def test_hartford(self):
+        pass
+
 if __name__ == "__main__":
     unittest.main()
